40f5aecf260249da6be5b7e1a9a747af79f16b04
Anton Boovyk <antonbovik@gmail.com>
2020-03-25 15:54:40 +0200

    Start

---
+++ src/com/company/Laboratorna_3_original.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Laboratorna_3_original {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(System.in);
+        while (sc != null){
+            System.out.println("Введіть речення або слово для отриманння розшифровки: ");
+            String[] splitted = new String[0];
+
+        }
+    }
+
+}
