02c1e04ffd69b3a4e39f82a3e282f6f32b60f94e
Anton Borovyk <54375146+vorlodka@users.noreply.github.com>
2020-03-25 21:24:13 +0200

    Update HashFunc.java

--- src/com/company/HashFunc.java
+++ src/com/company/HashFunc.java
@@ -4,7 +4,7 @@ package com.company;
 public class HashFunc<Key, Value> {
     int numberOfElem = 0;
     int capacityVar = 16;
-    double loadCoefVar = 0.75d;
+    double loadCoefVar = 0.8d;
     KeyValue[] links = new KeyValue[capacityVar];
 
 
