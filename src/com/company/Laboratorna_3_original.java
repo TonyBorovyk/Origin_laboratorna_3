b9e224b9ba84a781c492b7d8a7ec2271dba36716
MykolaOnyshchuk <54044502+MykolaOnyshchuk@users.noreply.github.com>
2020-03-25 22:39:27 +0200

    Add Dictionary method to Laboratorna_3_original

--- src/com/company/Laboratorna_3_original.java
+++ src/com/company/Laboratorna_3_original.java
@@ -22,4 +22,17 @@ public class Laboratorna_3_original {
             CipherWord(dict1,splitted);
         }
     }
+    public static HashFunc<String, String> Dictionary () throws FileNotFoundException {
+        File fVar = new File("dict_processed.txt");
+        Scanner sc = new Scanner(fVar);
+        HashFunc<String, String> dict = new HashFunc<>();
+        while (sc.hasNextLine()){
+            String s = sc.nextLine();
+            int i = s.indexOf(';');
+            String keyword = s.substring(0, i);
+            String cipher = s.substring(i+1);
+            dict.put(keyword, cipher);
+        }
+        return dict;
+    }
 }
