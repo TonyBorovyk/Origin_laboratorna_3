0c73f55ad223e39a56c7d464587d6723d3a98949
MykolaOnyshchuk <54044502+MykolaOnyshchuk@users.noreply.github.com>
2020-03-25 19:21:34 +0200

    Update HashFunc.java
    Add FindIndex and scanKeyValueData functions

--- src/com/company/HashFunc.java
+++ src/com/company/HashFunc.java
@@ -8,7 +8,20 @@ public class HashFunc<Key, Value> {
     KeyValue[] links = new KeyValue[capacityVar];
 
 
-}
 
+    int FindIndex(int hash, int capacityVar) {
+        return hash & (capacityVar - 1);
+    }
 
-
+    Value scanKeyValueData(int hash, Key hashKey, Value value, int ind) {
+        KeyValue<Key, Value> data = links[ind];
+        while (data != null) {
+            if (data.mHash == hash && hashKey.equals(data.mHashKey)) {
+                Value deltaValue = data.mValue;
+                data.mValue = value;
+                return deltaValue;
+            }
+            data = data.next;
+        }
+        return null;
+    }
