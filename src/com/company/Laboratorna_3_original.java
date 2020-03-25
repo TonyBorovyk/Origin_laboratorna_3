9fbac5315fcb0ae77749c23d904dde6d0ac880d7
MykolaOnyshchuk <54044502+MykolaOnyshchuk@users.noreply.github.com>
2020-03-25 22:17:12 +0200

    Some changes in Laboratorna_3_original

--- src/com/company/Laboratorna_3_original.java
+++ src/com/company/Laboratorna_3_original.java
@@ -7,12 +7,19 @@ import java.util.Scanner;
 public class Laboratorna_3_original {
 
     public static void main(String[] args) throws FileNotFoundException {
+        HashFunc<String, String>  dict1 = Dictionary();
         Scanner sc = new Scanner(System.in);
         while (sc != null){
             System.out.println("Введіть речення або слово для отриманння розшифровки: ");
             String[] splitted = new String[0];
-
+            if (sc.hasNextLine()){
+                String clause = sc.nextLine();
+                splitted = clause.split(" ");}
+            if(splitted.length == 0){
+                sc.close();
+                return;
+            }
+            CipherWord(dict1,splitted);
         }
     }
-
 }
