52e514b1ca24ebd57ef4c49060bfbf88022b323d
Anton Borovyk <54375146+vorlodka@users.noreply.github.com>
2020-03-27 20:38:34 +0200

    Update Laboratorna_3_original.java
    Fix

--- src/com/company/Laboratorna_3_original.java
+++ src/com/company/Laboratorna_3_original.java
@@ -7,38 +7,38 @@ import java.util.Scanner;
 public class Laboratorna_3_original {
 
     public static void main(String[] args) throws FileNotFoundException {
-        HashFunc<String, String>  dict1 = Dictionary();
+        Hash_function<String, String>  slovnyk1 = Slovnyk();
         Scanner sc = new Scanner(System.in);
         while (sc != null){
             System.out.println("Введіть речення або слово для отриманння розшифровки: ");
             String[] splitted = new String[0];
-            if (sc.hasNextLine()){
-                String clause = sc.nextLine();
-                splitted = clause.split(" ");}
-            if(splitted.length == 0){
-                sc.close();
+            if (scanner.hasNextLine()){
+                String sentence = scanner.nextLine();
+                words = sentence.split(" ");}
+            if(words.length==0){
+                scanner.close();
                 return;
             }
-            CipherWord(dict1,splitted);
+            shifr_word(slovnyk1,words);
         }
     }
-    public static HashFunc<String, String> Dictionary () throws FileNotFoundException {
+    public static Hash_function<String, String> Slovnyk () throws FileNotFoundException {
         File fVar = new File("dict_processed.txt");
         Scanner sc = new Scanner(fVar);
-        HashFunc<String, String> dict = new HashFunc<>();
+        Hash_function<String, String> slovnik = new Hash_function<>();
         while (sc.hasNextLine()){
             String s = sc.nextLine();
             int i = s.indexOf(';');
-            String keyword = s.substring(0, i);
-            String cipher = s.substring(i+1);
-            dict.put(keyword, cipher);
+            String keyWord = s.substring(0, i);
+            String shifrWord = s.substring(i+1);
+            slovnik.PutInfo(keyWord, shifrWord);
         }
-        return dict;
+        return slovnik;
     }
-    public static void CipherWord (HashFunc<String, String>  dict,String[] splitted){
-        for (String cipherWord : splitted) {
-            System.out.print(cipherWord.toUpperCase() + " : ");
-            System.out.println(dict.Get(cipherWord.toUpperCase()));
+    public static void shifr_word (Hash_function<String, String>  dictionary,String[] words){
+        for (String cipherWord : words) {
+            System.out.print(word.toUpperCase() + " : ");
+            System.out.println(dictionary.GetKey(word.toUpperCase()));
             System.out.println(" ");
         }
     }
